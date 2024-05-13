e77c3c058b82f22c04d9728d078d102dbc11aa3d
UtopicUnicorn <medvedevtimofei@gmail.com>
2024-05-13 12:39:57 +0700

    added overlay component

--- src/app/notifications/notifications.module.ts
+++ src/app/notifications/notifications.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main/main.component';
 import { HeaderComponent } from './header/header.component';
+import { OverlayComponent } from './overlay/overlay.component';
 
 @NgModule({
-  declarations: [HeaderComponent, MainComponent],
+  declarations: [HeaderComponent, MainComponent, OverlayComponent],
   imports: [CommonModule],
   exports: [MainComponent],
 })
