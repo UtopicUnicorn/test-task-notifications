832b602415b8dbd683c3789cc6c96226dbae2abc
Timofei <53641761+UtopicUnicorn@users.noreply.github.com>
2024-05-15 21:15:19 +0700

    fixed error with notifications vanish and changed styles
    * updated styles for header on mobile devices
    
    * created overlay content fullsize on mobile devices
    
    * changed position to static for mobile devices
    
    * changed default font
    
    * changed operator to fix notification auto delay
    
    * added trackBy function for notifications list
    
    * added shadow for notification and changed font-sizes
    
    * added trackby and changed style for overlay
    
    * readme update

--- src/app/root-store/notifications/notifications.effects.ts
+++ src/app/root-store/notifications/notifications.effects.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
-import { concatMap, mapTo, timer } from 'rxjs';
+import { mapTo, mergeMap, timer } from 'rxjs';
 import {
   NotificationActionInterface,
   NotificationsActionTypes,
@@ -11,7 +11,7 @@ export class NotificationsEffects {
   @Effect()
   save$ = this.actions$.pipe(
     ofType(NotificationsActionTypes.Add),
-    concatMap((action: NotificationActionInterface) =>
+    mergeMap((action: NotificationActionInterface) =>
       timer(5000).pipe(
         mapTo({
           type: NotificationsActionTypes.DeleteDisplay,
